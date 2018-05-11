d9bdf8f5898d1209e4eb027532484ef940bc9726
Ani Danielyan <anigani81@gmail.com>
2018-05-11 11:27:00 +0400

    add some T functionality

--- src/com/ani/gani/Main.java
+++ src/com/ani/gani/Main.java
@@ -3,7 +3,8 @@ package com.ani.gani;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
+		T varT = new T("Test");
+		System.out.println(varT.getName());
 
 	}
 
--- src/com/ani/gani/T.java
+++ src/com/ani/gani/T.java
@@ -2,4 +2,20 @@ package com.ani.gani;
 
 public class T {
 
+	private String name;
+
+	public T(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
